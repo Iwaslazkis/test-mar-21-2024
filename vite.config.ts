bdcaf4567f358ba211bfefece2d9ae6bbd6dff86
Your Name <you@example.com>
2024-04-09 02:11:51 +0000

    Add CD support

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({}) => {
+  const config = { 
+    plugins: [react()]
+  }
+
+  if (process.env.BUILD_COMBINED === "true" && process.env.CURRENT_BUILD === "storybook") {
+    return { ...config,
+      base: "/storybook"
+    }
+  }
+  return { ...config,
+    
+  }
 })
