d01269f0c9b5d691cdbc8ff7712d945466aff1a9
Your Name <you@example.com>
2024-04-09 14:37:40 +0000

    Fix prefix

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({}) => {
   const config = { 
-    plugins: [react()]
+    plugins: [react()],
+    base: process.env.BUILD_PATH_PREFIX || undefined
   }
 
-  if (process.env.BUILD_COMBINED === "true" && process.env.CURRENT_BUILD === "storybook") {
+  if (process.env.BUILD_COMBINED === "true" && process.env.BUILD_TYPE === "storybook") {
     return { ...config,
-      base: "/storybook"
+      base: config.base + "/storybook"
     }
   }
   return { ...config,
